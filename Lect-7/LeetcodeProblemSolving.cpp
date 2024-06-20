4545e1e3045305a1c94bf825def069095431b1a7
lakshay2808 <lakshay8219@gmail.com>
2024-06-21 00:03:09 +0530

    changes

--- Lect-7/LeetcodeProblemSolving.cpp
+++ Lect-7/LeetcodeProblemSolving.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
-int main ()
+int main()
 {
 
     // // Problem-1   Reverse Integer
@@ -22,21 +22,77 @@ int main ()
     // }
     // cout<<"Reversed number is : "<<ans;
 
+    // // Problem-2   Complement of base 10 integer
+    // int n, m;
+    // cin >> n;
+    // m = n;
+    // cout << "Original number is : " << n << endl;
+    // int mask = 0;
 
-    // Problem-2   1's Complement
+    // while (m != 0)
+    // {
+    //     mask = mask << 1 | 1;
+    //     m = m >> 1;
+    // }
+    // if (n == 0)
+    // {
+    //     cout << "Complement number is : 1" << endl;
+    // }
+    // else
+    // {
+    //     int ans = mask & (~n);
+    //     cout << "Complement number is : " << ans;
+    // }
+
+    // // Problem-3   check whether n is power of 2 or not  --> My Logic
+    // int n;
+    // cin >> n;
+    // cout << "Original number is : " << n << endl;
+    // bool fact=true;
+    // while (n != 1)
+    // {
+    //     if (n%2==0)
+    //     {
+    //         fact = true;
+    //     }
+    //     else
+    //     {
+    //         fact = false;
+    //         cout<<"Not power of 2"<<endl;
+    //         break;
+    //     }
+    //     n=n/2;
+    // }
+    // if (fact == true)
+    // {
+    //     cout << "Yes power of 2" << endl;
+    // }
+
+    // Problem-3   check whether n is power of 2 or not  --> Tut Logic
     int n;
-    cin>>n;
-    cout<<"Original number is : "<<n<<endl;
-    int ans=0,i=0;
-    while (n!=0)
+    cin >> n;
+    cout << "Original number is : " << n << endl;
+    bool fact = false;
+    int ans = 1;
+    for (int i = 0; i <= 30; i++)
     {
-        int bit = n&1;
-        int digit = ~bit;
-        ans=digit*pow(10,i)+ans;  
-        n=n>>1;
-        i++;
+        if (ans == n)
+        {
+            fact = true;
+            break;
+        }
+        if (ans < INT_MAX/2)
+        {
+            ans *= 2;   
+        }
     }
-    cout<<"Reversed number is : "<<ans;
- 
- return 0;
+    if (fact == true)
+    {
+        cout << "Yes Power of 2" << endl;
+    }
+    else
+    {
+        cout << "Not power of 2" << endl;
+    }
+    return 0;
 }
