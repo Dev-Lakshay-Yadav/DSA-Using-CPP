bfde970323c4f84e945a2567efbba1901141a859
lakshay2808 <lakshay8219@gmail.com>
2024-06-24 22:58:25 +0530

    Switch & functions

---
+++ Lect-8/Functions.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+// int func(int a, int b)
+// {
+//     int z = a + b;
+//     return z;
+// }
+
+
+
+//     // Even odd
+// bool isEven(int num){
+//     if(num % 2==0)
+//     {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+
+// // nCr factorial
+// int fact(int n)
+// {
+//     int fact =1;
+//     for (int i = 1; i <=n; i++)
+//     {
+//         fact = fact*i;
+//     }
+//     return fact;
+// }
+// int nCr (int n,int r)
+// {
+//     int ans = (fact(n)/((fact(r)*fact(n-r))));
+//     return ans;
+// }
+
+// int setBits(int a,b)
+// {
+//     int count =0;
+//     return count;
+// }
+
+// int fabonacci(int n){
+//     int ans=0;
+//     return ans;
+// }
+
+
+
+int main()
+{
+    // int a=3, b=4;
+    // cout<<"sum of a and b is : "<<func(a,b);
+
+
+    // // Even odd
+    // int num;
+    // cin>>num;
+    // if (isEven(num))
+    // {
+    //     cout<<num<<" is even";
+    // } 
+    // else
+    // {
+    //     cout<<num<<" is odd";
+    // }
+
+// // nCr factorial
+//     int n,r;
+//     cout<<"Enter value of n :";
+//     cin>>n;
+//     cout<<"Enter value of r :";
+//     cin>>r;
+//     cout<<"fact is : "<<nCr(n,r);
+
+
+    return 0;
+}
---
+++ Lect-8/SwitchStatement.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // // Switch Syntax
+    // int num = 2;
+    // switch (num)
+    // {
+    // case 1:
+    //     cout << "First";
+    //     break;
+    // case 2:
+    //     cout << "Second";
+    //     break;
+
+    // default:
+    //     cout << "default";
+    //     break;
+    // }
+
+    // char ch = 'A';
+    // switch (ch)
+    // {
+    // case 'A':
+    //     cout << "First";
+    //     break;
+    // case 'B':
+    //     cout << "Second";
+    //     break;
+
+    // default:
+    //     cout << "default";
+    //     break;
+    // }
+
+    // // Nested Switch
+    // int num=2;
+    // char ch = 'A';
+    // switch(num)
+    // {
+    //     case 1:
+    //         cout<<"First";
+    //         break;
+
+    //     case 2:
+    //         switch(ch)
+    //         {
+    //             case 'A':
+    //                 cout<<"Right O/P";
+    //                 break;
+    //             default:
+    //                 cout<<"Wrong";
+    //         }
+    //         break;
+    //     default :
+    //         cout<<"default";
+    // }
+
+    // //With Loop
+    // int n = 1, i = 1;
+    // while (i >= n)
+    // {
+    //     switch (n)
+    //     {
+    //     case 1:
+    //         cout << "First";
+    //         // goto jump;
+    //         // exit(0);
+    //         break;
+    //     case 2:
+    //         cout << "Second";
+    //         break;
+    //     default:
+    //         cout << "default";
+    //         break;
+    //     }
+    //     i++;
+    // }
+    // jump:
+    // cout<<"finish";
+
+    // // Continue with switch
+    // int n = 5, i = 1;
+    // while (i <= n)
+    // {
+    //     switch (i)
+    //     {
+    //     case 1:
+    //         cout << "First : "<<i<<endl;
+    //         continue;
+    //     case 2:
+    //         cout << "Second : "<<i<<endl;
+    //         break;
+    //     default:
+    //         cout << "default : "<<i<<endl;
+    //         break;
+    //     }
+    //     i++;
+    // }
+
+    // // Calculator using switch
+    // int a,b;
+    // char operation;
+    // cout<<"Enter value of a : ";
+    // cin>>a;
+    // cout<<"Enter value of b : ";
+    // cin>>b;
+    // cout<<"Enter operation you want to perform : ";
+    // cin>>operation;
+    // cout<<endl;
+    // switch (operation)
+    // {
+    // case '+':
+    //     cout<<"Sum of a and b is : "<<(a+b)<<endl;;
+    //     break;
+    // case '-':
+    //     cout<<"difference of a and b is : "<<(a-b)<<endl;;
+    //     break;
+    // case '*':
+    //     cout<<"Multiplication of a and b is : "<<(a*b)<<endl;;
+    //     break;
+    // case '/':
+    //     cout<<"Division of a and b is : "<<(a/b)<<endl;;
+    //     break;
+    // case '%':
+    //     cout<<"Modulus of a and b is : "<<(a%b)<<endl;;
+    //     break;
+    // default:
+    //     cout<<"Please enter a valid Operation!!"<<endl;
+    //     break;
+    // }
+    
+return 0;
+}
