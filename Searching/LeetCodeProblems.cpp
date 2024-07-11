1f6cdc3d9a50c06c83490c4c1969db5d10f9cd1b
lakshay8219@gmail.com <lakshay8219@gmail.com>
2024-07-11 16:51:56 +0530

    changes

--- Searching/LeetCodeProblems.cpp
+++ Searching/LeetCodeProblems.cpp
@@ -1,9 +1,8 @@
 
 // BINARY SEARCH LEETCODE PROBLEMS
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 // // here we use pair type function because we have to return 2 integer values together
 // // Que-1- Find First & Last Position of an Element in sorted Array
@@ -38,9 +37,8 @@ using namespace std;
 //             e = mid - 1;
 //         }
 //     }
-//     return make_pair(left,right);    
+//     return make_pair(left,right);
 // }
-
 
 // // Que-2- Find Occurance of an integer
 // int occuranceOfInteger(int arr[],int n,int key){
@@ -74,11 +72,10 @@ using namespace std;
 //             e = mid - 1;
 //         }
 //     }
-//     return count;    
+//     return count;
 // }
 
-
-// // Que-3- Find Peak Index in Mountain Array
+// Que-3- Find Peak Index in Mountain Array
 // int peakIndexInMountainArray(int arr[],int n){
 //     int s=0,e=n-1;
 //     while(s<e)
@@ -99,33 +96,75 @@ using namespace std;
 //     return -1;
 // }
 
+// // Que-4- Find Pivot in an Array
+// int pivotinArray(int arr[],int n){
+//     int s=0,e=n-1;
+//     while(s<e)
+//     {
+//         int mid = s + (e-s)/2;
+//         if (arr[mid] >= arr[0])
+//         {
+//             s = mid + 1;
+//         }
+//         else{
+//             e = mid;
+//         }
+//     }
+//     return s;
+// }
 
-// Que-4- Find Pivot in an Array
-int pivotinArray(int arr[],int n){
-    int s=0,e=n-1;
-    while(s<e)
+// Que-5- Find an element in a rotated sorted array
+int findKeyInRotatedArray(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    while (s < e)
     {
-        int mid = s + (e-s)/2;
-        if (arr[mid]>arr[mid-1] & arr[mid]>arr[mid+1])
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == key)
         {
-            return arr[mid];
+            return mid;
+        }
+        else if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+    }
+    int start,end;
+    if (key > arr[n - 1])
+    {
+        start = 0, end = s - 1;
+    }
+    else
+    {
+        start = s, end = n - 1;
+    }
+    while (start < end)
+    {
+        int mid = (start + end) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
             break;
         }
-        else if(arr[mid-1] > arr[mid]){
-            e = mid-1;
+        else if (arr[mid] > key)
+        {
+            // cout<<mid;
+            start = mid + 1;
         }
-        else{
-            s = mid+1;
+        else
+        {
+            // cout<<mid<<endl;
+            end = mid -1;
         }
     }
     return -1;
 }
 
-
-
-
-
-int main ()
+int main()
 {
     // Pair - It is used to make pair of 2 same or different type values together
 
@@ -145,13 +184,18 @@ int main ()
     // int arr[n] = {3,4,5,7,5,4,2,1};
     // cout<<"Highest number in array is : "<<peakIndexInMountainArray(arr,n);
 
+    // sorted and rotated array means {1,2,3,4} --> {4,1,2,3}
+    // iss case mein array 3&4 k bich se rotate hua hai to pivot element 4 ya 1 me se koi sa bhi le sakte hai
+    // but hum neeche vale solution mein 1 ko pivot element le rhe hai
+    // Que-4- Find Pivot in an Array --> Always give index of number 5 -->4-index as output because 5 is pivot element
+    // int n=7;
+    // int arr[n] = {6,8,10,17,5,3,1};
+    // cout<<"Smallest number in array is : "<<pivotinArray(arr,n);
 
-    // Que-3- Find Pivot in an Array
-    int n=8;
-    int arr[n] = {3,4,5,7,5,4,2,1};
-    cout<<"Highest number in array is : "<<pivotinArray(arr,n);
-    
-
-    
- return 0;
-} 
+    // Que-5- Find an element in a rotated sorted array
+    int n = 7;
+    int arr[n] = {6, 8, 10, 17, 5, 3, 1};
+    int key = 1;
+    cout << "Searched number in array is at index : " << findKeyInRotatedArray(arr, n, key);
+    return 0;
+}
